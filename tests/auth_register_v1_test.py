89488de9224ab2ced4eb1a617cb5f59e8794baca
Blake Morris <z5363525@unsw.edu.au>
2021-09-19 15:06:16 +1000

    Added stubs for auth_register_v1_test test cases

---
+++ tests/auth_register_v1_test.py
@@ -0,0 +1,20 @@
+import pytest
+
+from src.auth import auth_register_v1
+from src.error import InputError
+
+def test_valid_email():
+	pass
+
+def test_duplicate_email():
+	pass
+
+def test_password_length():
+	pass
+
+def test_name_first_length():
+	pass
+
+def test_name_last_length():
+	pass
+
