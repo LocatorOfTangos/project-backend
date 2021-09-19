dd0843dc65ce5583cdb4d8c427e577158fe961e1
Blake Morris <z5363525@unsw.edu.au>
2021-09-19 15:50:09 +1000

    Added tests for valid calles to auth_register_v1(), and testing that returned IDs are unique

--- tests/auth_register_v1_test.py
+++ tests/auth_register_v1_test.py
@@ -1,9 +1,26 @@
 import pytest
 
 from src.auth import auth_register_v1
+from src.auth import auth_login_v1
 from src.error import InputError
+from src.other import clear_v1
 
-# Tests for invalid inputs:
+def test_valid_register():
+	register_return = auth_register_v1("user@mail.com", "password", "firstname", "lastname")
+	login_return = auth_login_v1("user@mail.com", "password")
+	assert register_return == login_return
+
+	register2_return = auth_register_v1("user2@mail.com", "password", "firstname", "lastname")
+	login2_return = auth_login_v1("user2@mail.com", "password2")
+	assert register2_return == login2_return
+
+def test_user_id_unique():
+	used_ids = []
+	for i in range(30):
+		register_return = auth_register_v1(f"user{i}@mail.com", "password", "firstname", "lastname")
+		assert register_return not in used_ids
+		used_ids.append(register_return)
+
 def test_invalid_email():
 	with pytest.raises(InputError):
 	  	assert auth_register_v1("usermail.com", "password", "firstname", "lastname")
