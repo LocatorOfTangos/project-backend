81ebfcf58b86529f52c46bef3c4e831fc8397b9a
Blake Morris <z5363525@unsw.edu.au>
2021-09-19 15:28:41 +1000

    Added tests for invalid inputs to auth_register_v1_test

--- tests/auth_register_v1_test.py
+++ tests/auth_register_v1_test.py
@@ -3,18 +3,38 @@ import pytest
 from src.auth import auth_register_v1
 from src.error import InputError
 
-def test_valid_email():
-	pass
+# Tests for invalid inputs:
+def test_invalid_email():
+	with pytest.raises(InputError):
+	  	assert auth_register_v1("usermail.com", "password", "firstname", "lastname")
+	
+	with pytest.raises(InputError):
+	  	assert auth_register_v1("user@mail", "password", "firstname", "lastname")
+	
+	with pytest.raises(InputError):
+	  	assert auth_register_v1("@mail.com", "password", "firstname", "lastname")
+	
+	with pytest.raises(InputError):
+	  	assert auth_register_v1("user*@mail.com", "password", "firstname", "lastname")
 
 def test_duplicate_email():
-	pass
+	auth_register_v1("user@mail.com", "password", "firstname", "lastname")
+	with pytest.raises(InputError):
+		auth_register_v1("user@mail.com", "password2", "firstname2", "lastname2")
 
-def test_password_length():
-	pass
+def test_password_too_short():
+	with pytest.raises(InputError):
+	  	assert auth_register_v1("firstname@mail.com", "", "firstname", "lastname")
 
-def test_name_first_length():
-	pass
+	with pytest.raises(InputError):
+	  	assert auth_register_v1("firstname@mail.com", "12345", "firstname", "lastname")
 
-def test_name_last_length():
-	pass
+def test_first_name_empty():
+	with pytest.raises(InputError):
+	  	assert auth_register_v1("firstname@mail.com", "password", "", "lastname")
+
+def test_last_name_empty():
+	with pytest.raises(InputError):
+	  	assert auth_register_v1("firstname@mail.com", "password", "firstname", "")
+
 
