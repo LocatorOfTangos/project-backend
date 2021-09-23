fa4f947ecadc8847ffe9401205dff19e61d44ccd
Blake Morris <z5363525@unsw.edu.au>
2021-09-23 16:37:54 +1000

    Added test for clearing channels

--- tests/clear_v1_test.py
+++ tests/clear_v1_test.py
@@ -3,21 +3,22 @@ import pytest
 from src.other import clear_v1
 from src.auth import auth_register_v1
 from src.auth import auth_login_v1
+from src.channels import channels_create_v1
+from src.channel import channel_details_v1
 from src.error import InputError
 
-@pytest.fixture
-def users():
-	for i in range(20):
-		auth_register_v1(f"user{i}@mail.com", f"password{i}", f"firstname{i}", f"lastname{i}")
-	
-def test_users_clear(users):
-	# Check that users were added correctly
-	for i in range(20):
-		assert auth_login_v1(f"user{i}@mail.com", f"password{i}")['auth_user_id'] == i
-
-	# Check that users get removed by clear
+def test_users_clear():
 	clear_v1()
-	for i in range(20):
-		with pytest.raises(InputError):
-	  		assert auth_login_v1(f"user{i}@mail.com", f"password{i}")
+	u_id = auth_register_v1("user@mail.com", "password", "first", "last")['auth_user_id']
+	clear_v1()
+	with pytest.raises(InputError):
+		assert auth_login_v1("user@mail.com", "password")
 
+def test_channels_clear():
+	clear_v1
+	u_id = auth_register_v1("user@mail.com", "password", "first", "last")['auth_user_id']
+	c_id = channels_create_v1(u_id, "channel", True)['channel_id']
+	clear_v1()
+	u_id = auth_register_v1("user@mail.com", "password", "first", "last")['auth_user_id']
+	with pytest.raises(InputError):
+		assert channel_details_v1(uid, c_id)
