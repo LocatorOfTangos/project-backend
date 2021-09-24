54d84c36945f110fc1aab5829c25d3f8987674ac
Blake Morris <z5363525@unsw.edu.au>
2021-09-24 16:10:37 +1000

    Added tests

--- tests/clear_v1_test.py
+++ tests/clear_v1_test.py
@@ -7,18 +7,31 @@ from src.channels import channels_create_v1
 from src.channel import channel_details_v1
 from src.error import InputError
 
+@pytest.fixture(autouse=True)
+def clear_data():
+	clear_v1()
+
 def test_users_clear():
+	# Successfully register and login a user
+	user = auth_register_v1("user@mail.com", "password", "first", "last")
+	assert auth_login_v1("user@mail.com", "password")['auth_user_id'] == user
+
 	clear_v1()
-	u_id = auth_register_v1("user@mail.com", "password", "first", "last")['auth_user_id']
-	clear_v1()
+
 	with pytest.raises(InputError):
 		assert auth_login_v1("user@mail.com", "password")
 
 def test_channels_clear():
-	clear_v1
-	u_id = auth_register_v1("user@mail.com", "password", "first", "last")['auth_user_id']
-	c_id = channels_create_v1(u_id, "channel", True)['channel_id']
+	# Successfully register and login a user
+	user = auth_register_v1("user@mail.com", "password", "first", "last")
+	assert auth_login_v1("user@mail.com", "password")['auth_user_id'] == user
+
+	# Successfully create a channel and get its details
+	channel = channels_create_v1(user['auth_user_id'], "channel_name", True)
+	assert channel_details_v1(user['auth_user_id'], channel['channel_id'])['name'] == "channel_name"
+
 	clear_v1()
-	u_id = auth_register_v1("user@mail.com", "password", "first", "last")['auth_user_id']
+
 	with pytest.raises(InputError):
-		assert channel_details_v1(u_id, c_id)
+		assert channel_details_v1(user['auth_user_id'], channel['channel_id'])['name']
+
