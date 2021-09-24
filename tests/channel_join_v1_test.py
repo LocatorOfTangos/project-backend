9b9364a583edabed7b4872d98830fbc331f6f77e
Redmond Mobbs <imashellio@gmail.com>
2021-09-24 20:25:02 +1000

    Fixed syntax errors in channel_join_v1_test

--- tests/channel_join_v1_test.py
+++ tests/channel_join_v1_test.py
@@ -1,7 +1,7 @@
 import pytest
 
 from src.channel import channel_join_v1
-from src.channels import channel_create_v1
+from src.channels import channels_create_v1
 from src.auth import auth_register_v1
 from src.error import InputError, AccessError
 from src.other import clear_v1
@@ -12,22 +12,22 @@ def clear_data():
 	clear_v1()
 
 def test_return_type():
-    user1_id = auth_register(name1@email.com, password, firstname, lastname)
-    user2_id = auth_register(name2@email.com, password, firstname, lastname)
+    user1_id = auth_register_v1("name1@email.com", "password", "firstname", "lastname")
+    user2_id = auth_register_v1("name2@email.com", "password", "firstname", "lastname")
 
-    channel_id = channels_create_v1(user1_id, True)
+    channel_id = channels_create_v1(user1_id, "channelname", True)
 
     assert channel_join_v1(user2_id, channel_id) == {}
 
 def test_invalid_channel_id():
-    user1_id = auth_register(name1@email.com, password, firstname, lastname)
+    user1_id = auth_register_v1("name1@email.com", "password", "firstname", "lastname")
 
     # No channels to join
     with pytest.raises(InputError):
         assert channel_join_v1(user1_id, 0)
 
-    user2_id = auth_register(name2@email.com, password, firstname, lastname)
-    channels_create_v1(user1_id, True)
+    user2_id = auth_register_v1("name2@email.com", "password", "firstname", "lastname")
+    channels_create_v1(user1_id, "channelname", True)
 
     # A channel exists but the given channel_id is invalid
     with pytest.raises(InputError):
@@ -37,14 +37,14 @@ def test_invalid_channel_id():
         assert channel_join_v1(user1_id, 9999999999999999)
 
 def test_already_public_channel_member():
-    user1_id = auth_register(name1@email.com, password, firstname, lastname)
-    channel_id = channels_create_v1(user1_id, True)
+    user1_id = auth_register_v1("name1@email.com", "password", "firstname", "lastname")
+    channel_id = channels_create_v1(user1_id, "channelname", True)
 
     # Channel creator attempts to rejoin
     with pytest.raises(InputError):
         assert channel_join_v1(user1_id, channel_id)
 
-    user2_id = auth_register(name2@email.com, password, firstname, lastname)
+    user2_id = auth_register_v1("name2@email.com", "password", "firstname", "lastname")
     channel_join_v1(user2_id, channel_id)
 
     # Channel member attempts to rejoin
@@ -52,15 +52,15 @@ def test_already_public_channel_member():
         assert channel_join_v1(user2_id, channel_id)
 
 def test_already_private_channel_member():
-    user1_id = auth_register(name1@email.com, password, firstname, lastname)
-    channel1_id = channels_create_v1(user1_id, False)
+    user1_id = auth_register_v1("name1@email.com", "password", "firstname", "lastname")
+    channel1_id = channels_create_v1(user1_id, "channel1name", False)
 
     # Global Owner attempts to rejoin their own private channel
     with pytest.raises(InputError):
         assert channel_join_v1(user1_id, channel1_id)
 
-    user2_id = auth_register(name2@email.com, password, firstname, lastname)
-    channel2_id = channels_create_v1(user2_id, False)
+    user2_id = auth_register_v1("name2@email.com", "password", "firstname", "lastname")
+    channel2_id = channels_create_v1(user2_id, "channel2name", False)
     channel_join_v1(user1_id, channel2_id)
 
     # Global Owner attempts to rejoin a member's private channel
@@ -68,10 +68,10 @@ def test_already_private_channel_member():
         assert channel_join_v1(user1_id, channel2_id)
 
 def test_non_global_owner_joins_private_channel():
-    user1_id = auth_register(name1@email.com, password, firstname, lastname)
-    channel_id = channels_create_v1(user1_id, False)
+    user1_id = auth_register_v1("name1@email.com", "password", "firstname", "lastname")
+    channel_id = channels_create_v1(user1_id, "channelname", False)
 
-    user2_id = auth_register(name2@email.com, password, firstname, lastname)
+    user2_id = auth_register_v1("name2@email.com", "password", "firstname", "lastname")
 
     with pytest.raises(AccessError):
         assert channel_join_v1(user2_id, channel_id)
