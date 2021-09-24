8f16b7af125ae79d926999f49e892cf55b13580d
Blake Morris <z5363525@unsw.edu.au>
2021-09-24 21:20:10 +1000

    Corrected indentation error

--- src/other.py
+++ src/other.py
@@ -1,13 +1,13 @@
 from src.data_store import data_store
 
 def clear_v1():
-	'''
-	Clears the data store to reset the state of the program
+    '''
+    Clears the data store to reset the state of the program
 
-	No arguments
+    No arguments
     No exceptions
     No return value
-	'''
+    '''
     store = data_store.get()
     store['users'] = []
     store['channels'] = []
