d74ae17527706aa07f2b26b8d1c47680880f8dd0
Vu Luu <z5311086@ad.unsw.edu.au>
2021-09-28 02:58:20 +1000

    Add some tests for channel_details_v1

---
+++ tests/channel_details_v1_test.py
@@ -0,0 +1,64 @@
+import pytest
+
+from src.auth import auth_register_v1
+from src.error import InputError
+from src.other import clear_v1
+from src.channel import channel_details_v1, channel_join_v1
+from src.channels import channels_create_v1
+
+
+def test_invalid_channel_id():
+    '''
+    Test for invalid channel id
+    '''
+    clear_v1()
+    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
+    with pytest.raises(InputError):
+        assert(channel_details_v1(u_id, 3))
+   
+def test_valid_channel_id():
+    '''
+    Test for valid channel id and user id
+    '''
+    clear_v1()
+    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
+    channel_id = channels_create_v1(u_id, "test channel", True)
+    channel_detail = {}
+    channel_detail = channel_details_v1(u_id, channel_id)
+    assert(channel_detail) == {
+        'name': 'test channel',
+        'is_public': True,
+        'owner_members': [
+            {
+                'user_id': u_id,
+                'name_first': 'vu',
+                'name_last': 'luu',
+            },
+        ],
+        'all_members': [
+            {
+                'user_id': u_id ,
+                'name_first': 'vu',
+                'name_last': 'luu',
+            },
+        ],
+    }
+
+def test_user_not_authorised():
+    '''
+    Test when channel_id is valid and the authorised user is not a member of the channel
+    '''
+    clear_v1()
+    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
+    u2_id = auth_register_v1("secondtestemail@gmail.com", "password", "david", "smith")
+    channel_id = channels_create_v1(u_id, "test channel", True)
+    with pytest.raises(AccessError):
+        channel_details_v1(u2_id, channel_id)
+
+
+
+
+
+
+
+    
