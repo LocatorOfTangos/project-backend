a8380ac3435b9c6040415c3fab15335ecc31f271
Blake Morris <z5363525@unsw.edu.au>
2021-09-27 15:20:24 +1000

    Moved validation functions to new file

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,6 @@
 from src.error import AccessError, InputError
 from src.data_store import data_store
+from src.validation import user_is_member, valid_user_id, valid_channel_id
 
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
@@ -27,34 +28,6 @@ def channel_details_v1(auth_user_id, channel_id):
             }
         ],
     }
-
-# Returns true if channel_id refers to a valid channel, else false
-def valid_channel_id(channel_id):
-    store = data_store.get()
-    channels = store['channels']
-    for channel in channels:
-        if channel['channel_id'] == channel_id:
-            return True
-    return False
-
-# Returns true if u_id refers to a valid user, else false
-def valid_user_id(u_id):
-    store = data_store.get()
-    users = store['users']
-    for user in users:
-        if user['u_id'] == u_id:
-            return True
-    return False
-
-# Returns true if user u_id is a member of channel c_id, else false
-def user_is_member(u_id, c_id):
-    store = data_store.get()
-    users = store['channels'][c_id]['users']
-
-    for user in users:
-        if user['u_id'] == u_id:
-            return True
-    return False
 
 
 def channel_messages_v1(auth_user_id, channel_id, start):
