6a1497e1be601efa4cc46fa7438d54b0c7e131fc
Trissha <trisshavarman@gmail.com>
2021-09-28 18:19:36 +1000

    Replaced code with Blake's helper functions to identify valid user_id

--- src/channels.py
+++ src/channels.py
@@ -1,5 +1,6 @@
 from src.data_store import data_store
 from src.error import InputError, AccessError
+from src.validation import valid_user_id
 
 def channels_list_v1(auth_user_id):
     return {
@@ -27,20 +28,15 @@ def channels_create_v1(auth_user_id, name, is_public):
     channels = store['channels']
 
     #Check for valid user_id
-    users = store['users']
-    
-    for user in users:
-        if user['u_id'] == u_id:
-            return True
-        else:
-            raise AccessError('Invalid User ID')
-    
+    if not valid_user_id(auth_user_id):
+        raise AccessError("User ID does not exist")
+
     #Check for valid channel name
     if not 1 <= len(name) <= 20:
         raise InputError('Invalid Channel Name. Name must be between 1 to 20 characters')
     
     #Create channel_id
-    number_of_channels = len(channels)
+    channel_id = len(channels)
     
     channels_details = {
         'channel_id': channel_id,
@@ -48,7 +44,7 @@ def channels_create_v1(auth_user_id, name, is_public):
         'is_public': is_public,
         'owner_members': [auth_user_id],
         'all_members': [auth_user_id],
-        'messages': ,
+        'messages': []
     }
     
     store['channels'].append(channels_details)
