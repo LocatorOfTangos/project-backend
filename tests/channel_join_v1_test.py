fe063ce3c1dcd009762a76e65fed33231ff10131
Redmond Mobbs <imashellio@gmail.com>
2021-09-28 13:59:05 +1000

    Included a test function for a successful application of join

--- tests/channel_join_v1_test.py
+++ tests/channel_join_v1_test.py
@@ -19,6 +19,34 @@ def test_return_type():
 
     assert channel_join_v1(user2_id, channel_id) == {}
 
+def test_join_successful():
+    user1_id = auth_register_v1("name1@email.com", "password", "firstname", "lastname")
+    user2_id = auth_register_v1("name2@email.com", "password", "firstname", "lastname")
+
+    pubchannel_id = channels_create_v1(user1_id, "channelname", True)
+    channel_join_v1(user2_id, pubchannel_id)
+
+    privchannel_id = channels_create_v1(user2_id, "privatechannelname", False)
+    channel_join_v1(user1_id, privchannel_id)
+
+    # A user can join a public server
+    joined = False
+    for user in channel_details_v1(user1_id, pubchannel_id)['all_members']:
+        if user[u_id] == user2_id:
+            joined = True
+            break
+    
+    assert joined == True
+
+    # A global owner can join a private server
+    joined = False
+    for user in channel_details_v1(user2_id, privchannel_id)['all_members']:
+        if user[u_id] == user1_id:
+            joined = True
+            break
+    
+    assert joined == True
+
 def test_invalid_channel_id():
     user1_id = auth_register_v1("name1@email.com", "password", "firstname", "lastname")
 
