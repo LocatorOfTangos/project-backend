217885f377cbca951771c0a364b8a68dd2d546d6
tylergan <tgan2615@gmail.com>
2021-09-28 17:04:45 +1000

    First iteration of listall implementation

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,6 @@
+from src.data_store import data_store
+
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -8,15 +11,16 @@ def channels_list_v1(auth_user_id):
         ],
     }
 
+
 def channels_listall_v1(auth_user_id):
-    return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
-    }
+    channels = data_store.get()['channels']
+    owners_channels = [{
+        'channel_id': channel['channel_id'],
+        'name': channel['name']
+    } for channel in channels if auth_user_id in channel['owner_members']]
+
+    return owners_channels
+
 
 def channels_create_v1(auth_user_id, name, is_public):
     return {
