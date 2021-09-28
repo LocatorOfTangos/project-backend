e67c0194aa64d2371144851b942a52e7d271c7d2
Vu Luu <z5311086@ad.unsw.edu.au>
2021-09-28 17:59:53 +1000

    Skip some tests because channels_create is not fnished

--- tests/channel_details_v1_test.py
+++ tests/channel_details_v1_test.py
@@ -15,7 +15,8 @@ def test_invalid_channel_id():
     u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
     with pytest.raises(InputError):
         assert(channel_details_v1(u_id, 3))
-   
+
+@pytest.mark.skip(reason = "waiting for channels_create implementation")   
 def test_valid_channel_id():
     '''
     Test for valid channel id and user id
@@ -44,6 +45,7 @@ def test_valid_channel_id():
         ],
     }
 
+@pytest.mark.skip(reason = "waiting for channels_create implementation")
 def test_user_not_authorised():
     '''
     Test when channel_id is valid and the authorised user is not a member of the channel
@@ -53,7 +55,7 @@ def test_user_not_authorised():
     u2_id = auth_register_v1("secondtestemail@gmail.com", "password", "david", "smith")
     channel_id = channels_create_v1(u_id, "test channel", True)
     with pytest.raises(AccessError):
-        channel_details_v1(u2_id, channel_id)
+        assert(channel_details_v1(u2_id, channel_id))
 
 
 
