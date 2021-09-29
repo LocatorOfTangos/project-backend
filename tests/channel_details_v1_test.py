b8ab418cabbae68c10b139b0c251dbc1dc03d398
Vu Luu <z5311086@ad.unsw.edu.au>
2021-09-29 19:42:35 +1000

    Added more tests for channel_details_v1

--- tests/channel_details_v1_test.py
+++ tests/channel_details_v1_test.py
@@ -6,22 +6,32 @@ from src.other import clear_v1
 from src.channel import channel_details_v1, channel_join_v1
 from src.channels import channels_create_v1
 
+@pytest.fixture
+def clear():
+	clear_v1()
 
 def test_invalid_channel_id():
     '''
     Test for invalid channel id
     '''
-    clear_v1()
     u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
     with pytest.raises(InputError):
-        assert(channel_details_v1(u_id, 3))
+        assert(channel_details_v1(u_id, -1))
+
+def test_invalid_user_id():
+    '''
+    Test for valid channel id but invalid user id
+    '''
+    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
+    channel_id = channels_create_v1(u_id, "test channel", True)
+    with pytest.raises(InputError):
+        assert(channel_details_v1(-1, channel_id))
 
 @pytest.mark.skip(reason = "waiting for channels_create implementation")   
 def test_valid_channel_id():
     '''
     Test for valid channel id and user id
     '''
-    clear_v1()
     u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
     channel_id = channels_create_v1(u_id, "test channel", True)
     channel_detail = {}
@@ -50,12 +60,55 @@ def test_user_not_authorised():
     '''
     Test when channel_id is valid and the authorised user is not a member of the channel
     '''
-    clear_v1()
     u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
     u2_id = auth_register_v1("secondtestemail@gmail.com", "password", "david", "smith")
     channel_id = channels_create_v1(u_id, "test channel", True)
     with pytest.raises(AccessError):
         assert(channel_details_v1(u2_id, channel_id))
+
+@pytest.mark.skip(reason = "waiting for channels_create implementation")
+def test_multiple_user():
+    '''
+    Test when channel have multiple user and is private
+    '''
+    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
+    u2_id = auth_register_v1("secondtestemail@gmail.com", "password", "david", "smith")
+    u3_id = auth_register_v1("thirdemail@gmail.com", "password", "sam", "nguyen")
+
+    channel_id = channels_create_v1(u_id, "test channel", False)
+    channel_join_v1(u2_id, channel_id)
+    channel_join_v1(u3_id, channel_id)
+    channel_detail = {}
+
+    channel_detail = channel_details_v1(u_id, channel_id)
+    assert(channel_detail) == {
+        'name': 'test channel',
+        'is_public': False,
+        'owner_members': [
+            {
+                'user_id': u_id,
+                'name_first': 'vu',
+                'name_last': 'luu',
+            },
+        ],
+        'all_members': [
+            {
+                'user_id': u_id ,
+                'name_first': 'vu',
+                'name_last': 'luu',
+            },
+            {
+                'user_id': u2_id ,
+                'name_first': 'david',
+                'name_last': 'smith',
+            },
+            {
+                'user_id': u3_id ,
+                'name_first': 'sam',
+                'name_last': 'nguyen',
+            },
+        ],
+
 
 
 
