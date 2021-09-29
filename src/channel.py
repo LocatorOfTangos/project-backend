3adaf8d66b8c792a2ccf04c8941ff1c398619698
Vu Luu <z5311086@ad.unsw.edu.au>
2021-09-29 19:48:30 +1000

    Finish channel_details_v1 but haven't tested

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,6 @@
 from src.error import AccessError, InputError
 from src.data_store import data_store
-from src.validation import user_is_member, valid_user_id, valid_channel_id
+from src.validation import user_is_member, valid_user_id, valid_channel_id, get_user_details
 
 def channel_invite_v1(auth_user_id, channel_id, u_id):
 	return {
@@ -8,11 +8,6 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
 
 def channel_details_v1(auth_user_id, channel_id):
 
-	'''Raise input error for invalid channel id
-	Raise access error if auth_user_id is not in the channel
-	Create an empty list to store all members and owners.
-	Loop through channels in data_store. Find the matching channel_id. Return that channel dict
-	'''
 	# Check if channel is valid
 	if not valid_channel_id(channel_id):
 		raise InputError("Channel does not exist")
@@ -26,13 +21,45 @@ def channel_details_v1(auth_user_id, channel_id):
 		raise AccessError("User ID does not belong to a user")
 
 	# Implement the function
-	owner = []
+	owners = []
 	members = []
+	channel_details = {}
 	store = data_store.get()
 	channels = store['channels']
-	for channel in data[channels]:
-		if channel.get("channel_id") == channel_id:
+
+	for channel in channels:
+
+		# Find the channel_id in data_store
+		if channel_id == channel['channel_id']:
 			channel_name = channel.get('name')
+			status = channel['is_public']
+
+			# Add all members in channel list of all_members[] dictionary to members[] list
+			# Use get_user_details function to get user's details 
+			# from the data store.
+			for user_member in channel['all_members']:
+				temp_member = get_user_details(user_member)
+				members.append(temp_member)
+
+			# Add all owners in channel list of owner_member dictionary to owners[] list
+			for user_owner in channel['owner_members']:
+				temp_owner = get_user_details(user_owner)
+				owners.append(temp_owner)
+
+	channel_details = {
+		'name': channel_name,
+		'is_public': status,
+		'all_members': members,
+		'owner_members': owner,
+	}
+
+	return channel_details
+
+
+
+			
+
+
 			
 				
 
