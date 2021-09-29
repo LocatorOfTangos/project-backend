bb841fc3d412fbf1c916bf9e1ea510f7a5135203
Vu Luu <z5311086@ad.unsw.edu.au>
2021-09-29 21:59:22 +1000

    Fix user id not valid, added more tests and change expected output for the first letter of name

--- tests/channel_details_v1_test.py
+++ tests/channel_details_v1_test.py
@@ -1,12 +1,12 @@
 import pytest
 
-from src.auth import auth_register_v1
-from src.error import InputError
+from src.auth import auth_register_v1, auth_login_v1
+from src.error import InputError, AccessError
 from src.other import clear_v1
 from src.channel import channel_details_v1, channel_join_v1
 from src.channels import channels_create_v1
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def clear():
 	clear_v1()
 
@@ -14,26 +14,37 @@ def test_invalid_channel_id():
     '''
     Test for invalid channel id
     '''
-    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
+    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")['auth_user_id']
     with pytest.raises(InputError):
-        assert(channel_details_v1(u_id, -1))
+        assert(channel_details_v1(u_id, 3))
+
 
 def test_invalid_user_id():
     '''
     Test for valid channel id but invalid user id
     '''
-    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
-    channel_id = channels_create_v1(u_id, "test channel", True)
+    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")['auth_user_id']
+    channel_id = channels_create_v1(u_id, "test channel", True)['channel_id']
     with pytest.raises(InputError):
         assert(channel_details_v1(-1, channel_id))
 
-@pytest.mark.skip(reason = "waiting for channels_create implementation")   
+def test_user_not_authorised():
+    '''
+    Test when channel_id is valid and the authorised user is not a member of the channel
+    '''
+    clear_v1()
+    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")['auth_user_id']
+    u2_id = auth_register_v1("secondtestemail@gmail.com", "password", "david", "smith")['auth_user_id']
+    channel_id = channels_create_v1(u_id, "test channel", True)['channel_id']
+    with pytest.raises(AccessError):
+        assert(channel_details_v1(u2_id, channel_id))
+
 def test_valid_channel_id():
     '''
     Test for valid channel id and user id
     '''
-    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
-    channel_id = channels_create_v1(u_id, "test channel", True)
+    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")['auth_user_id']
+    channel_id = channels_create_v1(u_id, "test channel", True)['channel_id']
     channel_detail = {}
     channel_detail = channel_details_v1(u_id, channel_id)
     assert(channel_detail) == {
@@ -41,41 +52,32 @@ def test_valid_channel_id():
         'is_public': True,
         'owner_members': [
             {
-                'user_id': u_id,
-                'name_first': 'vu',
-                'name_last': 'luu',
+                'u_id': u_id,
+                'name_first': 'Vu',
+                'name_last': 'Luu',
             },
         ],
         'all_members': [
             {
-                'user_id': u_id ,
-                'name_first': 'vu',
-                'name_last': 'luu',
+                'u_id': u_id ,
+                'name_first': 'Vu',
+                'name_last': 'Luu',
             },
         ],
     }
 
-@pytest.mark.skip(reason = "waiting for channels_create implementation")
-def test_user_not_authorised():
-    '''
-    Test when channel_id is valid and the authorised user is not a member of the channel
-    '''
-    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
-    u2_id = auth_register_v1("secondtestemail@gmail.com", "password", "david", "smith")
-    channel_id = channels_create_v1(u_id, "test channel", True)
-    with pytest.raises(AccessError):
-        assert(channel_details_v1(u2_id, channel_id))
 
-@pytest.mark.skip(reason = "waiting for channels_create implementation")
-def test_multiple_user():
-    '''
-    Test when channel have multiple user and is private
-    '''
-    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")
-    u2_id = auth_register_v1("secondtestemail@gmail.com", "password", "david", "smith")
-    u3_id = auth_register_v1("thirdemail@gmail.com", "password", "sam", "nguyen")
 
-    channel_id = channels_create_v1(u_id, "test channel", False)
+@pytest.mark.skip(reason="channels_join not implemented")
+def test_multiple_users():
+    
+    #Test when channel have multiple user and is private
+    
+    u_id = auth_register_v1("testemail@gmail.com", "password", "vu", "luu")['auth_user_id']
+    u2_id = auth_register_v1("secondtestemail@gmail.com", "password", "david", "smith")['auth_user_id']
+    u3_id = auth_register_v1("thirdemail@gmail.com", "password", "sam", "nguyen")['auth_user_id']
+
+    channel_id = channels_create_v1(u_id, "test channel", False)['channel_id']
     channel_join_v1(u2_id, channel_id)
     channel_join_v1(u3_id, channel_id)
     channel_detail = {}
@@ -87,27 +89,28 @@ def test_multiple_user():
         'owner_members': [
             {
                 'user_id': u_id,
-                'name_first': 'vu',
-                'name_last': 'luu',
+                'name_first': 'Vu',
+                'name_last': 'Luu',
             },
         ],
         'all_members': [
             {
                 'user_id': u_id ,
-                'name_first': 'vu',
-                'name_last': 'luu',
+                'name_first': 'Vu',
+                'name_last': 'Luu',
             },
             {
                 'user_id': u2_id ,
-                'name_first': 'david',
-                'name_last': 'smith',
+                'name_first': 'David',
+                'name_last': 'Smith',
             },
             {
                 'user_id': u3_id ,
-                'name_first': 'sam',
-                'name_last': 'nguyen',
+                'name_first': 'Sam',
+                'name_last': 'Nguyen',
             },
-        ],
+        ]
+    }
 
 
 
