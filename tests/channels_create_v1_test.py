f2881e5dd7aadf09c5c4cc0590906b4cb1b4b4d8
Trissha <trisshavarman@gmail.com>
2021-09-29 16:11:34 +1000

    Added uniqueness tests to comply better with blackbox testing. Also added a test for a valid integer output and auth_user_id

--- tests/channels_create_v1_test.py
+++ tests/channels_create_v1_test.py
@@ -1,7 +1,7 @@
 import pytest
 from src.auth import auth_register_v1
 from src.channels import channels_create_v1
-from src.error import InputError
+from src.error import InputError, AccessError
 from src.other import clear_v1
 
 # Clears existing data for all tests
@@ -21,21 +21,32 @@ def test_channel_name_too_short():
 	with pytest.raises(InputError):
 		assert channels_create_v1(auth_user_id2, "", True)
 
-# Test for correct output and incrementing of channel ids
+def test_invalid_auth_id():
+	with pytest.raises(AccessError):
+		assert channels_create_v1(20, "channelname", True)
 
-def test_channel_id_one():
+# Test for correct output
+
+def test_channel_id_uniqueness():
+	used_channel_ids = set()
+
 	auth_user_id3 = auth_register_v1("player3@mail.com", "password", "firstname", "lastname")['auth_user_id']
-	assert channels_create_v1(auth_user_id3, "firstchannel", True) == {'channel_id': 0}
+	channel_id1 = channels_create_v1(auth_user_id3, "firstchannel", True)['channel_id']
+	used_channel_ids.add(channel_id1)
 
-def test_channel_id_multiple():
 	auth_user_id4 = auth_register_v1("player4@mail.com", "password", "firstname", "lastname")['auth_user_id']
-	assert channels_create_v1(auth_user_id4, "firstchannel", True) == {'channel_id': 0}
+	channel_id2 = channels_create_v1(auth_user_id4, "firstchannel", True)['channel_id']
+
+	assert channel_id2 not in used_channel_ids
+	used_channel_ids.add(channel_id2)
 
 	auth_user_id5 = auth_register_v1("player5@mail.com", "password", "firstname", "lastname")['auth_user_id']
-	assert channels_create_v1(auth_user_id5, "secondchannnel", True) == {'channel_id': 1}
+	channel_id3 = channels_create_v1(auth_user_id5, "firstchannel", True)['channel_id']
 
+	assert channel_id3 not in used_channel_ids
+
+def test_valid_integer_output():
 	auth_user_id6 = auth_register_v1("player6@mail.com", "password", "firstname", "lastname")['auth_user_id']
-	assert channels_create_v1(auth_user_id6, "thirdchannel", True) == {'channel_id': 2}
+	channel_id6 = channels_create_v1(auth_user_id6, "firstchannel", True)['channel_id']
+	assert isinstance(channel_id6, int)
 
-	auth_user_id7 = auth_register_v1("player7@mail.com", "password", "firstname", "lastname")['auth_user_id']
-	assert channels_create_v1(auth_user_id7, "fourthchannel", True) == {'channel_id': 3}
