93a974273e6147c9a77d63d937e34bb1262b84a0
Trissha <trisshavarman@gmail.com>
2021-10-01 15:18:42 +1000

    Added tests for auth_id and basic output

--- tests/channels_list_v1_test.py
+++ tests/channels_list_v1_test.py
@@ -1,7 +1,7 @@
 import pytest
 from src.auth import auth_register_v1
 from src.channels import channels_create_v1, channels_list_v1
-from src.error import InputError
+from src.error import InputError, AccessError
 from src.other import clear_v1
 
 # Clears existing data for all tests
@@ -11,37 +11,58 @@ def clear():
 
 # Tests for valid output for channels_list_v1_test.py
 
-def test_channels_list_v1():
-    user_1 = auth_register_v1("user1@gmail.com", "password1", "firstname", "lastname")
-    user_2 = auth_register_v1("user2@gmail.com", "password2", "firstname", "lastname")
+def test_channels_list_basic():
+    user_1 = auth_register_v1("user1@gmail.com", "password1", "firstname", "lastname")['auth_user_id']
+    channels_create_v1(user_1, "channel", True)
+
+    assert channels_list_v1(user_1) == {
+        'channels': [
+            {
+                'channel_id': 0,
+                'name': 'channel',
+            }
+        ]
+    }
+
+
+def test_channels_list_multiple():
+    user_1 = auth_register_v1("user1@gmail.com", "password1", "firstname", "lastname")['auth_user_id']
+    user_2 = auth_register_v1("user2@gmail.com", "password2", "firstname", "lastname")['auth_user_id']
 
     channels_create_v1(user_1, "channel", True)
     channels_create_v1(user_1, "channel2", True)
     channels_create_v1(user_2, "channel3", True)
     channels_create_v1(user_2, "channel4", True)
 
-    assert channels_list_v1(user_1) == 
-    {'channels': [
-        {
-            'id': 1,
-            'name': 'channel',
-        }
-        {
-            'id': 2,
-            'name': 'channel2',
-        }
-    ]}
+    assert channels_list_v1(user_1) == {
+        'channels': [
+            {
+                'channel_id': 0,
+                'name': 'channel',
+            },
+            {
+                'channel_id': 1,
+                'name': 'channel2',
+            }
+        ]
+    }
 
-    assert channels_list_v1(user_2) == 
-    {'channels': [
-        {
-            'id': 3,
-            'name': 'channel3',
-        }
-        {
-            'id': 4,
-            'name': 'channel4',
-        }
-    ]}
+    assert channels_list_v1(user_2) == {
+        'channels': [
+            {
+                'channel_id': 2,
+                'name': 'channel3',
+            },
+            {
+                'channel_id': 3,
+                'name': 'channel4',
+            }
+        ]
+    }
+
+# Tests for valid user ID
+def test_invalid_auth_id():
+	with pytest.raises(AccessError):
+		assert channels_list_v1(1234)
 
 
