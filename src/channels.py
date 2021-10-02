63207a3a343967a874c9740a37e10bbe728fcab1
Trissha <trisshavarman@gmail.com>
2021-10-02 14:37:22 +1000

    updated pydoc for channels_list and channels_create

--- src/channels.py
+++ src/channels.py
@@ -3,6 +3,20 @@ from src.error import InputError, AccessError
 from src.validation import valid_user_id
 
 def channels_list_v1(auth_user_id):
+    '''
+	Provides the list of channels with associated details that an authorised user is part of.
+
+	Arguments:
+		auth_user_id (integer)  - id of a registered user
+
+	Exceptions:
+		AccessError  - Occurs when:
+			> A user has not been registered and thus does not have a valid ID.
+
+	Return Value:
+		Returns a dictionary of a list of dictionaries containing 'channel_id'
+        and channel 'name'.
+	'''
 
     #check for valid user_id
     if not valid_user_id(auth_user_id):
@@ -42,6 +56,25 @@ def channels_listall_v1(auth_user_id):
 
 
 def channels_create_v1(auth_user_id, name, is_public):
+    '''
+	Creates and names a new channel that will be either public or private, automatically
+    adding the user who created it as the owner.
+
+	Arguments:
+		auth_user_id (integer)  - id of a registered user
+		name (string)           - name of the channel
+		is_public (boolean)     - indicates if channel is public (True) or private (False)
+
+	Exceptions:
+		InputError  - Occurs when:
+			> Channel name is not between 1 to 20 characters
+
+        AccessError - Occurs when:
+            > A user has not been registered and thus does not have a valid ID.
+
+	Return Value:
+		Returns a dictionary containing 'channel_id'
+	'''
 
     store = data_store.get()
     channels = store['channels']
