9319db27a9c559d01e5d91b4d76e59543646e0c5
Blake Morris <z5363525@unsw.edu.au>
2021-10-12 20:16:06 +1100

    Added additional tests requiring channel_details

--- tests/channel_invite_v2_test.py
+++ tests/channel_invite_v2_test.py
@@ -34,23 +34,28 @@ def invitee_id2():
     return auth_login_v2_request('invitee_id2@email.com', 'password').json()['auth_user_id']
 
 
+def check_added(token, channel, user_id):
+    members = channel_details_v2_request(token, channel).json()['all_members']
+    assert any(user['u_id'] == user_id for user in members)
+
+
 
 def test_successful_inv_public(inviter, public, invitee_id):
     assert channel_invite_v2_request(inviter, public, invitee_id).status_code == 200
-    # TODO: Check that they got added using channel_details
+    check_added(inviter, public, invitee_id)
 
 
 def test_successful_inv_private(inviter, private, invitee_id):
     assert channel_invite_v2_request(inviter, private, invitee_id).status_code == 200
-    # TODO: Check that they got added using channel_details
+    check_added(inviter, private, invitee_id)
 
 def test_multiple_members_invited(inviter, private, invitee_id, invitee_id2):
     assert channel_invite_v2_request(inviter, private, invitee_id).status_code == 200
-    assert channel_invite_v2_request(inviter, private, invitee_id2).status_code == 200 # Causes already added error
-    # TODO: Check that they got added using channel_details
+    assert channel_invite_v2_request(inviter, private, invitee_id2).status_code == 200
+    check_added(inviter, private, invitee_id)
+    check_added(inviter, private, invitee_id2)
 
 def test_invalid_auth_id(inviter, private, invitee_id):
-
     assert channel_invite_v2_request(1234, private, invitee_id).status_code == 403
 
 def test_invalid_invitee_id(inviter, private):
@@ -59,11 +64,9 @@ def test_invalid_invitee_id(inviter, private):
 def test_invalid_channel_id(inviter, private, invitee_id):
     assert channel_invite_v2_request(inviter, 12356487, invitee_id).status_code == 400
 
-
 def test_already_member(inviter, private, invitee_id):
     assert channel_invite_v2_request(inviter, private, invitee_id).status_code == 200
     assert channel_invite_v2_request(inviter, private, invitee_id).status_code == 400
-
 
 def test_already_member_multiple(inviter, private, invitee_id, invitee_id2):
     assert channel_invite_v2_request(inviter, private, invitee_id).status_code == 200
@@ -72,14 +75,13 @@ def test_already_member_multiple(inviter, private, invitee_id, invitee_id2):
     assert channel_invite_v2_request(inviter, private, invitee_id).status_code == 400
     assert channel_invite_v2_request(inviter, private, invitee_id2).status_code == 400
 
-
 def test_inviter_not_a_member(public, private, invitee_tkn, invitee_id2):
     assert channel_invite_v2_request(invitee_tkn, public, invitee_id2).status_code == 403
     assert channel_invite_v2_request(invitee_tkn, private, invitee_id2).status_code == 403
-
 
 def test_invite_transitivity(inviter, private, invitee_id, invitee_tkn, invitee_id2):
     assert channel_invite_v2_request(invitee_tkn, private, invitee_id2).status_code == 403
     assert channel_invite_v2_request(inviter, private, invitee_id).status_code == 200
     assert channel_invite_v2_request(invitee_tkn, private, invitee_id2).status_code == 200
-    # TODO: Check that they got added using channel_details
+    check_added(inviter, private, invitee_id)
+    check_added(inviter, private, invitee_id2)
