06c5715ab008d3bf518a7194da0cfbfda560c793
Blake Morris <z5363525@unsw.edu.au>
2021-10-13 15:33:12 +1100

    Updated handle tests to use http

--- tests/handle_test.py
+++ tests/handle_test.py
@@ -1,30 +1,28 @@
-from src.auth import auth_register_v1
-from src.channel import channel_details_v1, channel_join_v1
-from src.channels import channels_create_v1
-from src.other import clear_v1
 import pytest
+from src.make_request import *
+
 
 @pytest.fixture(autouse=True)
 def clear():
-	clear_v1()
+	clear_v1_request()
 
 def test_unique_names():
 	correct_handles = {"blakemorris", "redmondmobbs", "vuluu"}
 
 	# Create users
-	u_id1 = auth_register_v1("user1@mail.com", "password", "blake", "morris")['auth_user_id']
-	u_id2 = auth_register_v1("user2@mail.com", "password", "redmond", "mobbs")['auth_user_id']
-	u_id3 = auth_register_v1("user3@mail.com", "password", "vu", "luu")['auth_user_id']
+	u_id1 = auth_register_v2_request("user1@mail.com", "password", "blake", "morris").json()['token']
+	u_id2 = auth_register_v2_request("user2@mail.com", "password", "redmond", "mobbs").json()['token']
+	u_id3 = auth_register_v2_request("user3@mail.com", "password", "vu", "luu").json()['token']
 
 	# Create channel
-	c_id = channels_create_v1(u_id1, "channel", True)['channel_id']
+	c_id = channels_create_v2_request(u_id1, "channel", True).json()['channel_id']
 
 	# Add users
-	channel_join_v1(u_id2, c_id)
-	channel_join_v1(u_id3, c_id)
+	channel_join_v2_request(u_id2, c_id)
+	channel_join_v2_request(u_id3, c_id)
 
 	# Get members list
-	membs = channel_details_v1(u_id1, c_id)['all_members']
+	membs = channel_details_v2_request(u_id1, c_id).json()['all_members']
 
 	# Check handles are correct
 	assert membs[0]['handle_str'] in correct_handles
@@ -40,19 +38,19 @@ def test_long_names():
 	correct_handles = {"abcdefghijklmnopqrst", "qwertyuiopasdfghjklz", "mnbvcxzlkjhgfdsapoiu"}
 
 	# Create users
-	u_id1 = auth_register_v1("user1@mail.com", "password", "abcdefghijklmnopq", "rstuvwxyz")['auth_user_id']
-	u_id2 = auth_register_v1("user2@mail.com", "password", "qwertyuiopasdfg", "hjklzxcvbnm")['auth_user_id']
-	u_id3 = auth_register_v1("user3@mail.com", "password", "mnbvcxzlkjhgfds", "apoiuytrewq")['auth_user_id']
+	u_id1 = auth_register_v2_request("user1@mail.com", "password", "abcdefghijklmnopq", "rstuvwxyz").json()['token']
+	u_id2 = auth_register_v2_request("user2@mail.com", "password", "qwertyuiopasdfg", "hjklzxcvbnm").json()['token']
+	u_id3 = auth_register_v2_request("user3@mail.com", "password", "mnbvcxzlkjhgfds", "apoiuytrewq").json()['token']
 
 	# Create channel
-	c_id = channels_create_v1(u_id1, "channel", True)['channel_id']
+	c_id = channels_create_v2_request(u_id1, "channel", True).json()['channel_id']
 
 	# Add users
-	channel_join_v1(u_id2, c_id)
-	channel_join_v1(u_id3, c_id)
+	channel_join_v2_request(u_id2, c_id)
+	channel_join_v2_request(u_id3, c_id)
 
 	# Get members list
-	membs = channel_details_v1(u_id1, c_id)['all_members']
+	membs = channel_details_v2_request(u_id1, c_id).json()['all_members']
 
 	# Check handles are correct
 	assert membs[0]['handle_str'] in correct_handles
@@ -68,19 +66,19 @@ def test_same_names():
 	correct_handles = {"blakemorris", "blakemorris0", "blakemorris1"}
 
 	# Create users
-	u_id1 = auth_register_v1("user1@mail.com", "password", "blake", "morris")['auth_user_id']
-	u_id2 = auth_register_v1("user2@mail.com", "password", "blake", "morris")['auth_user_id']
-	u_id3 = auth_register_v1("user3@mail.com", "password", "blake", "morris")['auth_user_id']
+	u_id1 = auth_register_v2_request("user1@mail.com", "password", "blake", "morris").json()['token']
+	u_id2 = auth_register_v2_request("user2@mail.com", "password", "blake", "morris").json()['token']
+	u_id3 = auth_register_v2_request("user3@mail.com", "password", "blake", "morris").json()['token']
 
 	# Create channel
-	c_id = channels_create_v1(u_id1, "channel", True)['channel_id']
+	c_id = channels_create_v2_request(u_id1, "channel", True).json()['channel_id']
 
 	# Add users
-	channel_join_v1(u_id2, c_id)
-	channel_join_v1(u_id3, c_id)
+	channel_join_v2_request(u_id2, c_id)
+	channel_join_v2_request(u_id3, c_id)
 
 	# Get members list
-	membs = channel_details_v1(u_id1, c_id)['all_members']
+	membs = channel_details_v2_request(u_id1, c_id).json()['all_members']
 
 	# Check handles are correct
 	assert membs[0]['handle_str'] in correct_handles
@@ -96,19 +94,19 @@ def test_non_alnum():
 	correct_handles = {"blakemorris", "r3dm0ndm0bb5", "vuluu"}
 
 	# Create users
-	u_id1 = auth_register_v1("user1@mail.com", "password", "bla&ke", "m#o&rr*is")['auth_user_id']
-	u_id2 = auth_register_v1("user2@mail.com", "password", "r3d.....m0nd", "m0(b)b5")['auth_user_id']
-	u_id3 = auth_register_v1("user3@mail.com", "password", "vu____", "l/u/u")['auth_user_id']
+	u_id1 = auth_register_v2_request("user1@mail.com", "password", "bla&ke", "m#o&rr*is").json()['token']
+	u_id2 = auth_register_v2_request("user2@mail.com", "password", "r3d.....m0nd", "m0(b)b5").json()['token']
+	u_id3 = auth_register_v2_request("user3@mail.com", "password", "vu____", "l/u/u").json()['token']
 
 	# Create channel
-	c_id = channels_create_v1(u_id1, "channel", True)['channel_id']
+	c_id = channels_create_v2_request(u_id1, "channel", True).json()['channel_id']
 
 	# Add users
-	channel_join_v1(u_id2, c_id)
-	channel_join_v1(u_id3, c_id)
+	channel_join_v2_request(u_id2, c_id)
+	channel_join_v2_request(u_id3, c_id)
 
 	# Get members list
-	membs = channel_details_v1(u_id1, c_id)['all_members']
+	membs = channel_details_v2_request(u_id1, c_id).json()['all_members']
 
 	# Check handles are correct
 	assert membs[0]['handle_str'] in correct_handles
@@ -124,19 +122,19 @@ def test_long_duplicate_names():
 	correct_handles = {"abcdefghijklmnopqrst", "abcdefghijklmnopqrst0", "abcdefghijklmnopqrst1"}
 
 	# Create users
-	u_id1 = auth_register_v1("user1@mail.com", "password", "abcdefghijklmnopq", "rstuvwxyz")['auth_user_id']
-	u_id2 = auth_register_v1("user2@mail.com", "password", "abcdefghijklmnopq", "rstuvwxyz")['auth_user_id']
-	u_id3 = auth_register_v1("user3@mail.com", "password", "abcdefghijklmnopq", "rstuvwxyz")['auth_user_id']
+	u_id1 = auth_register_v2_request("user1@mail.com", "password", "abcdefghijklmnopq", "rstuvwxyz").json()['token']
+	u_id2 = auth_register_v2_request("user2@mail.com", "password", "abcdefghijklmnopq", "rstuvwxyz").json()['token']
+	u_id3 = auth_register_v2_request("user3@mail.com", "password", "abcdefghijklmnopq", "rstuvwxyz").json()['token']
 
 	# Create channel
-	c_id = channels_create_v1(u_id1, "channel", True)['channel_id']
+	c_id = channels_create_v2_request(u_id1, "channel", True).json()['channel_id']
 
 	# Add users
-	channel_join_v1(u_id2, c_id)
-	channel_join_v1(u_id3, c_id)
+	channel_join_v2_request(u_id2, c_id)
+	channel_join_v2_request(u_id3, c_id)
 
 	# Get members list
-	membs = channel_details_v1(u_id1, c_id)['all_members']
+	membs = channel_details_v2_request(u_id1, c_id).json()['all_members']
 
 	# Check handles are correct
 	assert membs[0]['handle_str'] in correct_handles
@@ -152,19 +150,19 @@ def test_duplicate_after_truncate():
 	correct_handles = {"abcdefghijklmnopqrst", "abcdefghijklmnopqrst0", "abcdefghijklmnopqrst1"}
 
 	# Create users
-	u_id1 = auth_register_v1("user1@mail.com", "password", "abcdefghijklmnopq", "rstabc")['auth_user_id']
-	u_id2 = auth_register_v1("user2@mail.com", "password", "abcdefghijklmnopq", "rstdef")['auth_user_id']
-	u_id3 = auth_register_v1("user3@mail.com", "password", "abcdefghijklmnopq", "rstghi")['auth_user_id']
+	u_id1 = auth_register_v2_request("user1@mail.com", "password", "abcdefghijklmnopq", "rstabc").json()['token']
+	u_id2 = auth_register_v2_request("user2@mail.com", "password", "abcdefghijklmnopq", "rstdef").json()['token']
+	u_id3 = auth_register_v2_request("user3@mail.com", "password", "abcdefghijklmnopq", "rstghi").json()['token']
 
 	# Create channel
-	c_id = channels_create_v1(u_id1, "channel", True)['channel_id']
+	c_id = channels_create_v2_request(u_id1, "channel", True).json()['channel_id']
 
 	# Add users
-	channel_join_v1(u_id2, c_id)
-	channel_join_v1(u_id3, c_id)
+	channel_join_v2_request(u_id2, c_id)
+	channel_join_v2_request(u_id3, c_id)
 
 	# Get members list
-	membs = channel_details_v1(u_id1, c_id)['all_members']
+	membs = channel_details_v2_request(u_id1, c_id).json()['all_members']
 
 	# Check handles are correct
 	assert membs[0]['handle_str'] in correct_handles
