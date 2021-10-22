57667b29fb970f12143a3601a508249bc1c95b49
Vu Luu <z5311086@ad.unsw.edu.au>
2021-10-22 17:30:01 +1100

    Wrote some tests for dm_list_v1

--- tests/dm_list_v1_test.py
+++ tests/dm_list_v1_test.py
@@ -17,7 +17,7 @@ def user1():
 def user2():
 	return auth_register_v2_request("testemail3@gmail.com", "password", "sam", "nguyen").json()['auth_user_id']
 
-def dm_list_empty(user):
+def test_dm_list_empty(owner):
     assert dm_list_v1_request(owner) == {'dms': []}
 
 def one_dm_list(owner):
@@ -25,67 +25,69 @@ def one_dm_list(owner):
 
     assert dm_list_v1_request(owner) == {'dms':[
         {
-        'dm_id': dm
+        'dm_id': dm,
         'name': 'vuluu'
         }]
     }
 
-def all_dm_list(owner, user1, user2):
+def test_all_dm_list(owner, user1, user2):
     dm_1 = dm_create_v1_request(owner, [user1]).json()['dm_id']
     dm_2 = dm_create_v1_request(owner, [user1, user2]).json()['dm_id']
 
     assert dm_list_v1_request(owner) == {'dms': [
         {
-        'dm_id': dm_1
+        'dm_id': dm_1,
         'name': 'davidsmith, vuluu'
         },
         {
-        'dm_id': dm_2
+        'dm_id': dm_2,
         'name': 'davidsmith, samnguyen, vuluu'
         }]
     }
 
-def some_dm_list(owner, user1, user2):
+def test_some_dm_list(owner, user1, user2):
+    # owner is in dm_1 and dm_2
     # user 1 is in dm_2 and dm_3 but not in dm_1
-    user1_token = auth_login_v2_request('testemail2@mail.com', 'password').json()['token']
-    user2_token = auth_login_v2_request('testemail3@mail.com', 'password').json()['token']
+    # user 2 is in all dms
+    user1_token = auth_login_v2_request('testemail2@gmail.com', 'password').json()['token']
+    user2_token = auth_login_v2_request('testemail3@gmail.com', 'password').json()['token']
     dm_1 = dm_create_v1_request(owner, [user2]).json()['dm_id']
     dm_2 = dm_create_v1_request(owner, [user1, user2]).json()['dm_id']
     dm_3 = dm_create_v1_request(user1_token, [user2]).json()['dm_id']
 
     assert dm_list_v1_request(user1_token) == {'dms': [
         {
-        'dm_id': dm_2
+        'dm_id': dm_2,
         'name': 'davidsmith, samnguyen, vuluu'
         },
         {
-        'dm_id': dm_3
+        'dm_id': dm_3,
         'name': 'davidsmith, samnguyen'
         }]
     }
 
     assert dm_list_v1_request(owner) == {'dms': [
         {
-        'dm_id': dm_1
+        'dm_id': dm_1,
         'name': 'samnguyen, vuluu'
         },
         {
-        'dm_id': dm_2
+        'dm_id': dm_2,
         'name': 'davidsmith, samnguyen, vuluu'
         }]
     }
 
     assert dm_list_v1_request(user2_token) == {'dms': [
         {
-        'dm_id': dm_1
+        'dm_id': dm_1,
         'name': 'samnguyen, vuluu'
         },
         {
-        'dm_id': dm_2
+        'dm_id': dm_2,
         'name': 'davidsmith, samnguyen, vuluu'
         },
         {
-        'dm_id': dm_3
+        'dm_id': dm_3,
         'name': 'davidsmith, samnguyen'
         }]
     }
