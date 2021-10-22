fc62e32b016387f0531df9d0e0dc93c3f1949620
tylergan <tgan2615@gmail.com>
2021-10-22 16:19:02 +1100

    Added implementation to the server

--- src/server.py
+++ src/server.py
@@ -18,7 +18,7 @@ from src.auth import auth_register_v1, auth_login_v1, auth_logout_v1
 from src.channels import channels_create_v1, channels_listall_v1, channels_list_v1
 from src.other import clear_v1
 from src.channel import channel_join_v1, channel_details_v1, channel_invite_v1, channel_messages_v1
-from src.dm import dm_create_v1
+from src.dm import dm_create_v1, dm_remove_v1
 from src.user import user_profile_v1
 from src.message import message_edit_v1, message_send_v1
 
@@ -149,6 +149,13 @@ def message_edit():
 def dm_create():
     data = request.get_json()
     resp = dm_create_v1(**data)
+    return dumps(resp)
+
+
+@APP.route('/dm/remove/v1', methods=['DELETE'])
+def dm_remove():
+    data = request.get_json()
+    resp = dm_remove_v1(**data)
     return dumps(resp)
 
 ########### User ############
