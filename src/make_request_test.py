c5f89a9d1197a97296309f73904d80d45cd4112e
Blake Morris <z5363525@unsw.edu.au>
2021-10-22 17:45:10 +1100

    tests for dm_messages

--- src/make_request_test.py
+++ src/make_request_test.py
@@ -124,3 +124,10 @@ def users_all_v1_request(token):
 	return requests.get(config.url + 'users/all/v1', params={
 		'token': token
 	})
+
+def dm_messages_v1_request(token, dm_id, start):
+	return requests.get(config.url + 'dm/message/v1', params={
+		'token': token,
+		'dm_id': dm_id,
+		'start': start
+	})
