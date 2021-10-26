2b78dedf9dc23130cb40e21c99b95c61bffd043b
Blake Morris <z5363525@unsw.edu.au>
2021-10-26 18:24:58 +1100

    Implemented search (using v2 definition of message dict)

--- tests/search_v1_test.py
+++ tests/search_v1_test.py
@@ -13,6 +13,7 @@ def user1():
 def user2():
 	return auth_register_v2_request("u2@mail.com", "password", "first", "last").json()['token']
 
+@pytest.fixture
 def channel1(user1):
 	ch = channels_create_v2_request(user1, "channel1", True).json()['channel_id']
 	message_send_v1_request(user1, ch, "hello world")
@@ -20,6 +21,7 @@ def channel1(user1):
 	message_send_v1_request(user1, ch, "defghij")
 	return ch
 
+@pytest.fixture
 def channel2(user2):
 	ch = channels_create_v2_request(user2, "channel1", True).json()['channel_id']
 	message_send_v1_request(user2, ch, "goodbye world")
@@ -27,10 +29,12 @@ def channel2(user2):
 	message_send_v1_request(user2, ch, "defghij")
 	return ch
 
+@pytest.fixture
 def dm(user1, user2):
-	dm = dm_create_v1_request(user1, [user2]).json()['dm_id']
-	message_send_v1_request(user1, dm, "the world is flat")
-	message_send_v1_request(user2, dm, "you're an idiot")
+	u2id = auth_login_v2_request("u2@mail.com", "password").json()['auth_user_id']
+	dm = dm_create_v1_request(user1, [u2id]).json()['dm_id']
+	message_senddm_v1_request(user1, dm, "the world is flat")
+	message_senddm_v1_request(user2, dm, "you're an idiot")
 
 
 
@@ -40,23 +44,24 @@ def test_status(user1, channel1):
 
 def test_one_result(user1, channel1):
 	messages = search_v1_request(user1, "hello").json()['messages']
-	assert {m['messsage'] for m in messages} == {"hello world"}
+	print(messages)
+	assert {m['message'] for m in messages} == {"hello world"}
 
 def test_multiple_results(user1, channel1):
 	messages = search_v1_request(user1, "defg").json()['messages']
-	assert {m['messsage'] for m in messages} == {"abcdefg", "defghij"}
+	assert {m['message'] for m in messages} == {"abcdefg", "defghij"}
 
 def test_other_channels(user1, channel1, channel2):
 	messages = search_v1_request(user1, "world").json()['messages']
-	assert {m['messsage'] for m in messages} == {"hello world"}
+	assert {m['message'] for m in messages} == {"hello world"}
 
 def test_dm(user1, dm):
 	messages = search_v1_request(user1, "world").json()['messages']
-	assert {m['messsage'] for m in messages} == {"the world is flat"}
+	assert {m['message'] for m in messages} == {"the world is flat"}
 
 def test_dm_and_channel(user1, dm, channel1):
 	messages = search_v1_request(user1, "world").json()['messages']
-	assert {m['messsage'] for m in messages} == {"the world is flat", "hello world"}
+	assert {m['message'] for m in messages} == {"the world is flat", "hello world"}
 
 def test_empty_search(user1):
 	assert search_v1_request(user1, "").status_code == 400
