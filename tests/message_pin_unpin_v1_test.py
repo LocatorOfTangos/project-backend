2d2abd8d579bf3017051c3ca34a2fd8426ef47d3
tylergan <tgan2615@gmail.com>
2021-11-04 14:59:51 +1100

    Fixed some test cases and pushing to test if it is working.

--- tests/message_pin_unpin_v1_test.py
+++ tests/message_pin_unpin_v1_test.py
@@ -8,7 +8,7 @@ def clear():
 # USERS
 @pytest.fixture
 def user():
-	return auth_register_v2_request('e@mail.com', "psword", "first", "last").json()['token']
+	return auth_register_v2_request('u1@mail.com', "psword", "first", "last").json()['token']
 
 @pytest.fixture
 def user2():
@@ -33,15 +33,19 @@ def d_msg(user, dm):
 	return message_senddm_v1_request(user, dm, "Hello DM! Pin this, please.").json()['message_id']
 
 # TESTS
-def msg_pin_status_test(user, d_msg, c_msg):
+def test_msg_pin_status(user, d_msg, c_msg):
     assert message_pin_v1_request(user, c_msg).status_code == 200
     assert message_pin_v1_request(user, d_msg).status_code == 200
 
-def not_valid_msg_id_test(user2, d_msg, c_msg):
+def test_not_valid_msg_id(user2, d_msg, c_msg):
     assert message_pin_v1_request(user2, c_msg).status_code == 400
     assert message_pin_v1_request(user2, d_msg).status_code == 400
 
-def not_owner_test(user, user2, channel, d_msg, c_msg):
+def test_not_owner(user, user2, channel, d_msg, c_msg):
     user2_id = auth_login_v2_request('u2@mail.com', 'psword').json()['auth_user_id']
     channel_invite_v2_request(user, channel, user2_id)
     message_pin_v1_request(user2, c_msg).status_code == 403
+
+# def test_pin_channel_msg(user, channel, c_msg):
+#     channel_messages_v2_request(user, channel, 0).json()['messages'][0]
+    
