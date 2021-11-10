79a7f8126ad52bb54e7052001df543aabc279752
Blake Morris <z5363525@unsw.edu.au>
2021-11-10 21:27:11 +1100

    Added tests checking the shared message

--- tests/message_share_v1_test.py
+++ tests/message_share_v1_test.py
@@ -24,8 +24,8 @@ def dm(user):
 	return dm_create_v1_request(user, []).json()['dm_id']
 
 @pytest.fixture
-def message(user, channel):
-	return message_send_v1_request(user, channel, "The world is flat").json()['message_id']
+def message(user, channel1):
+	return message_send_v1_request(user, channel1, "The world is flat").json()['message_id']
 
 
 def test_status(user, message, channel2):
@@ -37,15 +37,27 @@ def test_channel_and_dm_invalid(user, message):
 def test_channel_and_dm_not_neg1(user, message, channel2, dm):
 	assert message_share_v1_request(user, message, "Hi", channel2, dm).status_code == 400
 
-def test_invalid_message(user, channel):
-	assert message_share_v1_request(user, 12345, "Hi", channel).status_code == 400
+def test_invalid_message(user, channel1):
+	assert message_share_v1_request(user, 12345, "Hi", channel1).status_code == 400
 
-def test_too_long(user, channel, message):
-	assert message_share_v1_request(user, message, "a" * 1000, channel, -1).status_code == 200
-	assert message_share_v1_request(user, message, "a" * 1001, channel, -1).status_code == 400
+def test_too_long(user, channel1, message):
+	assert message_share_v1_request(user, message, "a" * 1000, channel1, -1).status_code == 200
+	assert message_share_v1_request(user, message, "a" * 1001, channel1, -1).status_code == 400
 
 def test_not_member_channel(user2, channel2, message):
 	assert message_share_v1_request(user, message, "Hi", channel2, -1).status_code == 403
 
 def test_not_member_dm(user, dm, message):
 	assert message_share_v1_request(user, message, "Hi", -1, dm).status_code == 403
+
+def test_shared_message_channel(user, channel2, message):
+	assert message_share_v1_request(user, message, "Look at this idiot", channel2, -1).status_code == 200
+	msg = channel_messages_v2_request(user, channel2, 0).json()['messages'][0]['message']
+	assert "Look at this idiot" in msg
+	assert "The world is flat" in msg
+
+def test_shared_message_dm(user, dm, message):
+	assert message_share_v1_request(user, message, "Truer words were never spoken", -1, dm).status_code == 200
+	msg = dm_messages_v1_request(user, dm, 0),json()['messages'][0]['message']
+	assert "Truer words were never spoken" in msg
+	assert "The world is flat" in msg
