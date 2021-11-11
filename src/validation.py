17ee5c099192c20f965f694582cfa7ad756d2f3e
Vu Luu <z5311086@ad.unsw.edu.au>
2021-11-12 01:46:04 +1100

    Added reset_code validation check

--- src/validation.py
+++ src/validation.py
@@ -140,3 +140,8 @@ def pin_message(u_id, m_id, pin_mode=True):
         msg['is_pinned'] = False
 
     return msg['is_pinned']
+
+def reset_code_is_valid(reset_code):
+    store = data_store.get()
+    return any(code['reset_code'] == reset_code for code in store['reset_code'])
+
